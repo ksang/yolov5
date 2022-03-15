62c40d421259af27ddebcb103797013c63332403
ksang <kaitoy@qq.com>
2022-03-15 15:36:47 +0800

    fix preproc

--- static_quantization.py
+++ static_quantization.py
@@ -1,11 +1,11 @@
 import os
 import torch
+import argparse
 import numpy as np
 from PIL import Image
 
 import onnxruntime
 from onnxruntime.quantization import quantize_static, CalibrationDataReader
-
 
 class DataReader(CalibrationDataReader):
     def __init__(self, calibration_image_folder, augmented_model_path='augmented_model.onnx'):
@@ -21,13 +21,12 @@ class DataReader(CalibrationDataReader):
         if self.preprocess_flag:
             self.preprocess_flag = False
             session = onnxruntime.InferenceSession(self.augmented_model_path, providers=self.providers)
-            (_, height, width, _) = session.get_inputs()[0].shape
-            nhwc_data_list = preprocess_func(self.image_folder, height, width, size_limit=0)
+            (_, _, height, width) = session.get_inputs()[0].shape
+            nchw_data_list = preprocess_func(self.image_folder, height, width, size_limit=0)
             input_name = session.get_inputs()[0].name
-            self.datasize = len(nhwc_data_list)
-            self.enum_data_dicts = iter([{input_name: nhwc_data} for nhwc_data in nhwc_data_list])
+            self.datasize = len(nchw_data_list)
+            self.enum_data_dicts = iter([{input_name: nchw_data} for nchw_data in nchw_data_list])
         return next(self.enum_data_dicts, None)
-
 
 def preprocess_func(images_folder, height, width, size_limit=0):
     '''
@@ -39,6 +38,7 @@ def preprocess_func(images_folder, height, width, size_limit=0):
     return: list of matrices characterizing multiple images
     '''
     image_names = os.listdir(images_folder)
+    image_names = [ i for i in image_names if i.endswith("jpg")]
     if size_limit > 0 and len(image_names) >= size_limit:
         batch_filenames = [image_names[i] for i in range(size_limit)]
     else:
@@ -47,29 +47,29 @@ def preprocess_func(images_folder, height, width, size_limit=0):
 
     for image_name in batch_filenames:
         image_filepath = images_folder + '/' + image_name
-        pillow_img = Image.new("RGB", (width, height))
+        # pillow_img = Image.new("RGB", (width, height))
+        img = cv2.imread(image_filepath)
         pillow_img.paste(Image.open(image_filepath).resize((width, height)))
-        input_data = np.float32(pillow_img) - \
-        np.array([123.68, 116.78, 103.94], dtype=np.float32)
+        # input_data = np.float32(pillow_img) - \
+        # np.array([123.68, 116.78, 103.94], dtype=np.float32)
         # input_data = np.array(pillow_img).astype('int64')
         nhwc_data = np.expand_dims(input_data, axis=0)
-        unconcatenated_batch_data.append(nhwc_data)
+        nchw_data = np.transpose(nhwc_data, (0, 3, 1, 2))
+        unconcatenated_batch_data.append(nchw_data)
     batch_data = np.concatenate(np.expand_dims(unconcatenated_batch_data, axis=0), axis=0)
     return batch_data
 
-
 def main():
+    parser = argparse.ArgumentParser(description='Do static quantization for YOLOv5 onnx models.')
+    parser.add_argument('-i', '--input-model', type=str, default="yolov5s.onnx", help='Input ONNX model path.')
+    parser.add_argument('-o', '--output-model', type=str, default="yolov5s-static-quantized.onnx", help='Output model path.')
+    parser.add_argument('-c', '--calibration-data', type=str, default="datasets/coco/images/val2017/", help='Images folder for data calibration.')
+    args = parser.parse_args()
 
-    input_model_path = 'yolov5s.onnx'
-    output_model_path = 'yolov5s-static-quantized.onnx'
-    calibration_dataset_path = '../datasets/coco/images/val2017/'
+    dr = DataReader(args.calibration_data)
+    quantize_static(args.input_model, args.output_model, dr)
 
-    # static quantization
-    dr = DataReader(calibration_dataset_path)
-    quantize_static(input_model_path, output_model_path, dr)
-
-    print('Calibrated and quantized model saved.')
-
+    print('Calibrated and quantized model saved to: {}'.format(args.output_model))
 
 if __name__ == '__main__':
     main()
